a85d0de9f7ab9d229c24f96af8e10b06d2dfb60b
Mulekotd <joaopedro.gama0675@gmail.com>
2025-11-11 17:37:23 -0300

    Adiciona a solução do problema 2303 e 2305 do beecrowd

---
+++ beecrowd/2305.py
@@ -0,0 +1,27 @@
+L, C, M, N = map(int, input().split())
+matrix = [[0] * (C + 1)]
+
+for _ in range(L):
+    matrix.append([0] + list(map(int, input().split())))
+
+for line in range(1, L + 1):
+    total = 0
+    
+    for col in range(1, C + 1):
+        total += matrix[line][col]
+        matrix[line][col] = total + matrix[line-1][col]
+
+acc_prefix = 0
+for i in range(M, L + 1):
+    for j in range(N, C + 1):
+        curr     = matrix[i][j]
+        diagonal = matrix[i-M][j-N]
+        line     = matrix[i-M][j]
+        colune   = matrix[i][j-N]
+        
+        partial = curr + diagonal - line - colune
+        
+        if partial > acc_prefix:
+            acc_prefix = partial
+
+print(acc_prefix)
