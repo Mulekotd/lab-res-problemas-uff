f35a4dace736381e50c420dc2a3d8efd5773f309
Mulekotd <joaopedro.gama0675@gmail.com>
2025-11-12 00:15:38 -0300

    Adiciona algoritmo de move zeros

---
+++ algorithms/move_zeros.py
@@ -0,0 +1,24 @@
+def move_zeros(v):
+    n = len(v)
+
+    non_zero = []
+    for i in range(n):
+        if v[i] != 0:
+            non_zero.append(v[i])
+
+    m = len(non_zero)
+
+    i = 0
+    while i < n:
+        for num in non_zero:
+            v[i] = num
+            i += 1
+        
+        for _ in range(m - 1, n - 1):
+            v[i] = 0
+            i += 1
+
+    return v
+
+print(move_zeros([0, 1, 3, 0, 12, 0, 2]))
+print(move_zeros([0, 0, 0, 0, 1]))
